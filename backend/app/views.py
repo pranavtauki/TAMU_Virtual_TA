fcdd3b2cbaae5effe118cfeb51476fd2f5d117e9
Pranav <pranavtaukari@gmail.com>
2023-07-03 14:31:16 -0700

    Added client code

---
+++ backend/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+def ChatAPI(request):
+    return HttpResponse("hello world!")
